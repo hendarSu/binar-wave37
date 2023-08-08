77b962e80294170bd840a914e56063eb49100a8a
mohagungsolihin <mohagungsolihin@gmail.com>
2023-08-08 13:44:25 +0700

    update manipulasi-string

--- mohagungsolihin/manipulasi-string.js
+++ mohagungsolihin/manipulasi-string.js
@@ -1,3 +1,45 @@
-const paragraf = "Sabrina adalah seorang web developer di Bingle";
+/**
+ * replaceAll() adalah Mengganti string dengan pola tertentu menjadi string lain.
+ */
 
-console.log(paragraf.replaceAll('Sabrina','Mas Gun'));
+const paragraf = "Sabrina adalah seorang web developer di Bingle";
+console.log(paragraf.replace('Sabrina','Mas Gun'));
+
+/**
+ * substring() adalah Mengambil beberapa karakter string dari index awal sampai sebelum index akhir.
+ */
+
+let abjad = "abcdefghij";
+console.log(abjad.substring(0, 5));
+
+/**
+ * toLowerCase() / toUpperCase adalah Mengganti string tertentu ke huruf kecil atau huruf kapital.
+ */
+
+let nama = "Muhammad Agung Solihin";
+let hurufKecil = nama.toLowerCase();
+let hurufBesar = nama.toUpperCase();
+console.log(hurufKecil);
+console.log(hurufBesar);
+
+/**
+ * trim() adalah Menghilangkan whitespace di awal dan akhir sebuah string.
+ */
+
+let alamat = "    Perumahan Panjibuwono Cluster Brawijaya      ";
+console.log(alamat.trim());
+
+
+/**
+ * length() adalah Menghasilkan panjang atau banyak karakter dari sebuah string.
+ */
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(text.length);
+
+/**
+ * slice() adalah Mengambil beberapa karakter string dari index awal sampai sebelum index akhir.
+ */
+
+let negara = "Indonesia Merdeka";
+console.log(negara.slice(0, 9));
